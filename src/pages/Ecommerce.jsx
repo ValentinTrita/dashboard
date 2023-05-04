28a3bc2962994cbda641ebb3f091e2964316734f
ValentinTrita <tritavalentin@icloud.com>
2023-05-04 17:47:10 +0300

    commit 04.05

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, SparkLine } from '../components';
 const Ecommerce = () => {
   return (
     <div>Ecommerce</div>
