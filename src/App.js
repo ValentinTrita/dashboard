cc244e7ef25044dc5b39f77e0f3943267d7bcc85
ValentinTrita <tritavalentin@icloud.com>
2023-06-06 17:12:37 +0300

    commit2

--- src/App.js
+++ src/App.js
@@ -42,6 +42,8 @@ const App = () => {
              <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                 <Navbar />
              </div>
+
+              <ThemeSettings />
             
 
             <div>
--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 const ThemeSetttings = () => {
   return (
