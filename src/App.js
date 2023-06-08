3c4a79fe63023fd513e2b07786cbe43e670ad227
ValentinTrita <tritavalentin@icloud.com>
2023-06-08 16:49:21 +0300

    6/8/2023

--- src/App.js
+++ src/App.js
@@ -12,15 +12,16 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-    const { activeMenu } =useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
   return (
     <div>
         <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
             <div className="fixed right-4 bottom-4" style={{ zIndex: '1000'  }}>
                 <TooltipComponent content="Settings" position="Top">
-                <button type="button" className="text-3x1 p-3 hover:drop-shadow-x1
+                <button type="button" className="text-3xl p-3 hover:drop-shadow-xl
                  hover:bg-light-gray text-white"
+                 onClick={() => setThemeSettings(true)}
                  style={{ background: 'blue', borderRadius: '50%' }}>
                     <FiSettings />
                 </button>
@@ -43,10 +44,9 @@ const App = () => {
                 <Navbar />
              </div>
 
-              <ThemeSettings />
+             <div>
+              { themeSettings && <ThemeSettings />}
             
-
-            <div>
                 <Routes>
                     {/* Dashboard */}
                     <Route path="/" element={<Ecommerce />} />
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,11 +11,28 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
    const [activeMenu, setActiveMenu] = useState (true);
    const [isClicked, setIsClicked] = useState (initialState);
-   const [screenSize, setScreenSize] = useState(undefined);
+   const [screenSize, setScreenSize] = useState (undefined);
+   const [currentColor, setCurrentColor] = useState ('#03C9D7');
+   const [currentMode, setCurrentMode] = useState ('Light');
+   const [themeSettings, setThemeSettings] = useState (false);
+   
+   const setMode = (e) => {
+    setCurrentMode(e.target.value);
+
+    localStorage.setItem('themeMode', e.target.value);
+   }
+
+   const setColor = (e) => {
+    setCurrentColor(e.target.value);
+
+    localStorage.setItem('colorMode', e.target.value);
+   }
 
    const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true});
    }
+
+   
     return (
         <StateContext.Provider 
         value={{ 
@@ -26,6 +43,9 @@ export const ContextProvider = ({ children }) => {
             handleClick,
             screenSize,
             setScreenSize,
+            currentColor, currentMode,
+            setCurrentColor, setCurrentMode,
+            themeSettings, setThemeSettings,
          }}
         >
           {children}
